3eb8d5f9c8234ad7b695bc3f8bb5e4c1c42c71bb
Harrison Goldstein <hgoldstein95@gmail.com>
2019-08-28 10:12:14 -0400

    Finish default tour init

--- src/config.rs
+++ src/config.rs
@@ -26,16 +26,24 @@ pub fn get_default_tours() -> Result<Vec<Tour>> {
         &fs::read_to_string(config_path()).context(ErrorKind::FailedToReadIndex)?,
     )
     .context(ErrorKind::FailedToParseIndex)?;
+    collect_tours(config.dirs.clone())
+}
+
+fn collect_tours(mut stack: Vec<AbsolutePathBuf>) -> Result<Vec<Tour>> {
     let mut tours = vec![];
-    // TODO: Make recursive
-    for dir in config.dirs {
+    while let Some(dir) = stack.pop() {
         let entries = dir
             .as_path_buf()
             .read_dir()
             .context(ErrorKind::FailedToParseIndex)?;
         for entry in entries {
             let path = entry.context(ErrorKind::FailedToReadIndex)?.path();
-            if path.extension().and_then(OsStr::to_str) == Some(".tour") {
+            if path.is_dir() {
+                stack.push(
+                    AbsolutePathBuf::new(path)
+                        .expect("read_dir should always return entries with absolute paths"),
+                );
+            } else if path.extension().and_then(OsStr::to_str) == Some("tour") {
                 let tour = serialize::parse_tour(
                     &fs::read_to_string(path).context(ErrorKind::FailedToReadTour)?,
                 )
@@ -45,4 +53,65 @@ pub fn get_default_tours() -> Result<Vec<Tour>> {
         }
     }
     Ok(tours)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::collect_tours;
+    use crate::types::path::AbsolutePathBuf;
+    use crate::types::Tour;
+    use std::fs;
+    use std::path::Path;
+    use tempdir::TempDir;
+
+    fn write_basic_tour(path: &Path) {
+        // This is super hacky, but it doesn't need to be robust. This is just to make sure I can
+        // create tour files arbitrarily far down a potentially non-existant directory tree.
+        if !path.parent().unwrap().exists() {
+            fs::create_dir_all(path.parent().unwrap()).unwrap();
+        }
+        fs::write(
+            path,
+            crate::serialize::serialize_tour(Tour {
+                generator: 0,
+                id: "TOURID".to_owned(),
+                title: "My first tour".to_owned(),
+                description: "".to_owned(),
+                stops: vec![],
+                protocol_version: "1.0".to_owned(),
+                repositories: vec![].into_iter().collect(),
+            })
+            .unwrap(),
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn collect_tours_works() {
+        let temp_dir = TempDir::new("collect_tours_works").unwrap();
+        write_basic_tour(&temp_dir.path().join("example.tour"));
+        let tours =
+            collect_tours(vec![AbsolutePathBuf::new(temp_dir.into_path()).unwrap()]).unwrap();
+        assert_eq!(tours[0].generator, 0);
+        assert_eq!(tours[0].title, "My first tour");
+        assert_eq!(tours[0].stops.len(), 0);
+    }
+
+    #[test]
+    fn collect_tours_recursively() {
+        let temp_dir = TempDir::new("collect_tours_recursive").unwrap();
+        write_basic_tour(&temp_dir.path().join("example1.tour"));
+        write_basic_tour(&temp_dir.path().join("between").join("example2.tour"));
+        write_basic_tour(
+            &temp_dir
+                .path()
+                .join("two")
+                .join("down")
+                .join("example3.tour"),
+        );
+        write_basic_tour(&temp_dir.path().join("between").join("example4.tour"));
+        let tours =
+            collect_tours(vec![AbsolutePathBuf::new(temp_dir.into_path()).unwrap()]).unwrap();
+        assert_eq!(tours.len(), 4);
+    }
 }
